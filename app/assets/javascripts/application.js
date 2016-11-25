60b48c762ad0b256d09fd1491db38fc668e48e4e
Gen Sasaki <sasakigen11@gmail.com>
2016-11-25 22:26:03 +0900

    Add s3 config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require dropzone
 //= require bootstrap-sprockets
 //= require toastr
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120005735) do
+ActiveRecord::Schema.define(version: 20161125115123) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "home_type"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20161120005735) do
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "listings", ["user_id"], name: "index_listings_on_user_id"
